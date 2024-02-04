e2098d419c2c634f8abddf6a20921f602538c39e
robertmartinsen <robertmartinsen2000@gmail.com>
2024-02-04 13:36:38 +0100

    contact page done

--- uta/src/App.js
+++ uta/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom"
 import Layout from "./components/layout/Layout"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Contact from "./pages/Contact"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/About" element={<About />} />
+          <Route path="/Contact" element={<Contact />} />
         </Route>
       </Routes>
     </div>
