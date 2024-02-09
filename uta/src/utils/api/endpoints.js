9769396b3ff5f7adcf471cfa26578b7e73942548
robertmartinsen <robertmartinsen2000@gmail.com>
2024-02-09 18:14:01 +0100

    endpoints

--- uta/src/utils/api/endpoints.js
+++ uta/src/utils/api/endpoints.js
@@ -1,2 +1,4 @@
 export const YT_URL = "https://www.googleapis.com/youtube/v3/"
 export const YT_VIDEO_URL = `${YT_URL}videos?`
+
+export const YT_ALL_VIDEOES_URL = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails&maxResults=60&playlistId=`
--- uta/src/utils/api/fetchAllVideoes.js
+++ uta/src/utils/api/fetchAllVideoes.js
@@ -1,5 +1,4 @@
-// Import necessary constants and functions
-import { YT_VIDEO_URL } from "./endpoints.js"
+import { YT_ALL_VIDEOES_URL } from "./endpoints.js"
 
 const CHANNEL_ID = "UCmsUw_9yP8JVbuXZ6bTDyyQ"
 const UPLOAD_ID = "UUmsUw_9yP8JVbuXZ6bTDyyQ"
@@ -7,25 +6,21 @@ const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY
 
 export const fetchAllVideos = async () => {
   try {
-    // Construct the API URL for fetching videos from a channel
-    const apiUrl = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails&maxResults=60&playlistId=${UPLOAD_ID}&key=${YOUTUBE_API_KEY}`
+    const apiUrl = `${YT_ALL_VIDEOES_URL}${UPLOAD_ID}&key=${YOUTUBE_API_KEY}`
 
-    // Make the API request
+
     const response = await fetch(apiUrl)
 
-    // Check if the request was successful
     if (!response.ok) {
       throw new Error(`Failed to fetch video data. Status: ${response.status}`)
     }
 
-    // Parse the response data
     const data = await response.json()
 
-    // Extract video information from the response
     const videos = []
 
     for (const videoItem of data.items) {
-      // Get video details using the videos endpoint
+
       const videoDetailsResponse = await fetch(
         `https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&id=${videoItem.contentDetails.videoId}&key=${YOUTUBE_API_KEY}`
       )
@@ -38,11 +33,10 @@ export const fetchAllVideos = async () => {
 
       const videoDetailsData = await videoDetailsResponse.json()
 
-      // Check if the video duration is at least 1 minute and 1 second
       const videoDuration = parseISO8601Duration(
         videoDetailsData.items[0].contentDetails.duration
       )
-      const isLongEnough = videoDuration >= 61 // 1 minute and 1 second
+      const isLongEnough = videoDuration >= 61
 
       if (isLongEnough) {
         videos.push({
@@ -60,7 +54,6 @@ export const fetchAllVideos = async () => {
   }
 }
 
-// Helper function to parse ISO 8601 duration format
 const parseISO8601Duration = (duration) => {
   const match = duration.match(/PT(\d+H)?(\d+M)?(\d+S)?/)
 
