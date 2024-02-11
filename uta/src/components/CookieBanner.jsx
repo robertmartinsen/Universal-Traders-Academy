416442c5346b3283144292da1ff984436bc53215
robertmartinsen <robertmartinsen2000@gmail.com>
2024-02-11 11:04:04 +0100

    cookies files added and created

---
+++ uta/src/components/CookieBanner.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react"
+import Cookies from "js-cookie"
+import classes from "../styles/components/CookieBanner.module.scss"
+
+const CookieBanner = () => {
+  const [showBanner, setShowBanner] = useState(!Cookies.get("acceptCookies"))
+
+  const handleAcceptCookies = () => {
+    Cookies.set("acceptCookies", true, { expires: 365 })
+    setShowBanner(false)
+  }
+
+  if (!showBanner) {
+    return null
+  }
+
+  return (
+    <div className={classes.cookieBanner}>
+      <div className={classes.textBox}>
+        <p>
+          This website uses cookies. Read more about our privacy & policy{" "}
+          <a
+            href="https://www.termsfeed.com/live/3923072f-c700-4ea6-8256-8ffd9c5cc0e9"
+            target="_blank"
+          >
+            here
+          </a>
+          .
+        </p>
+      </div>
+      <div className={classes.btnBox}>
+        <button onClick={handleAcceptCookies}>Alright</button>
+      </div>
+    </div>
+  )
+}
+
+export default CookieBanner
